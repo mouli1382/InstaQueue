5ddf73b46d1745a4f3f2451759fc27072ae5523b
mouli1382 <mouli1382@gmail.com>
2016-10-13 18:33:40 +0530

    Moved the class as per the package structure.

--- app/src/client/java/in/gm/instaqueue/adapter/TokenRecyclerViewHolder.java
+++ app/src/client/java/in/gm/instaqueue/adapter/TokenRecyclerViewHolder.java
@@ -1,9 +1,7 @@
 package in.gm.instaqueue.adapter;
 
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
