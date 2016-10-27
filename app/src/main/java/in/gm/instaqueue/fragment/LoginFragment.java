81d97e33cf3e9b322cc4cc8aede6f53870d310b7
Narasimha Gonapa <gnm444@gmail.com>
2016-10-27 11:24:30 +0530

    gauth integration
    gauth integration

--- app/src/main/java/in/gm/instaqueue/fragment/LoginFragment.java
+++ app/src/main/java/in/gm/instaqueue/fragment/LoginFragment.java
@@ -2,6 +2,7 @@ package in.gm.instaqueue.fragment;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -13,6 +14,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.digits.sdk.android.AuthCallback;
@@ -20,21 +22,29 @@ import com.digits.sdk.android.AuthConfig;
 import com.digits.sdk.android.Digits;
 import com.digits.sdk.android.DigitsException;
 import com.digits.sdk.android.DigitsSession;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
 import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.TwitterCore;
 
 import java.io.IOException;
+import java.util.concurrent.Executor;
 
 import in.gm.instaqueue.BuildConfig;
+import in.gm.instaqueue.R;
 import in.gm.instaqueue.activity.LandingActivity;
 import in.gm.instaqueue.activity.OnBoardingActivity;
 import in.gm.instaqueue.backend.myApi.MyApi;
@@ -42,15 +52,108 @@ import in.gm.instaqueue.model.User;
 import in.gm.instaqueue.prefs.SharedPrefs;
 import in.gm.instaqueue.util.AppConstants;
 import io.fabric.sdk.android.Fabric;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 
-public class LoginFragment extends BaseFragment {
+public class LoginFragment extends BaseFragment implements
+        GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener {
 
     private static final String TAG = "LoginFragment";
     public static final String LOGIN_FRAGMENT_TAG = LoginFragment.class.getName();
     private String mPhoneNumber;
     private String mLineNumber;
+    private GoogleApiClient mGoogleApiClient;
+    private int RC_SIGN_IN = 1000;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     public LoginFragment() {
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        Log.d(LOGIN_FRAGMENT_TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(LOGIN_FRAGMENT_TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(getActivity(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            Log.w(LOGIN_FRAGMENT_TAG, "Authentication success");
+                            Toast.makeText(getActivity(), "Authentication success.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            // ...
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                // ...
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(LOGIN_FRAGMENT_TAG, "onConnectionFailed:" + connectionResult);
     }
 
     @Override
@@ -61,9 +164,35 @@ public class LoginFragment extends BaseFragment {
             TelephonyManager tMgr = (TelephonyManager) _application.getSystemService(Context.TELEPHONY_SERVICE);
             mLineNumber = tMgr.getLine1Number();
         }
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(LOGIN_FRAGMENT_TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(LOGIN_FRAGMENT_TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity().getApplicationContext())
+                .enableAutoManage(getActivity() /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
 
-        TwitterAuthConfig authConfig = new TwitterAuthConfig(BuildConfig.TWITTER_KEY, BuildConfig.TWITTER_SECRET);
-        Fabric.with(_application, new TwitterCore(authConfig), new Digits.Builder().build());
+        //TwitterAuthConfig authConfig = new TwitterAuthConfig(BuildConfig.TWITTER_KEY, BuildConfig.TWITTER_SECRET);
+        //Fabric.with(_application, new TwitterCore(authConfig), new Digits.Builder().build());
 
     }
 
@@ -75,7 +204,7 @@ public class LoginFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        AuthCallback authCallback = new AuthCallback() {
+        /*AuthCallback authCallback = new AuthCallback() {
             @Override
             public void success(DigitsSession session, String phoneNumber) {
                 Toast.makeText(getActivity(), "Authentication successful for "
@@ -95,14 +224,21 @@ public class LoginFragment extends BaseFragment {
             public void failure(DigitsException exception) {
                 Log.d("Digits", "Sign in with Digits failure", exception);
             }
-        };
+        };*/
 
-        AuthConfig.Builder authConfigBuilder = new AuthConfig.Builder()
-                .withAuthCallBack(authCallback)
-                .withPhoneNumber("+91" + (!TextUtils.isEmpty(mLineNumber) ? mLineNumber : ""));
-        Digits.authenticate(authConfigBuilder.build());
+       // AuthConfig.Builder authConfigBuilder = new AuthConfig.Builder()
+              //  .withAuthCallBack(authCallback)
+              //  .withPhoneNumber("+91" + (!TextUtils.isEmpty(mLineNumber) ? mLineNumber : ""));
+//        Digits.authenticate(authConfigBuilder.build());
 
-        return null;
+
+        //return null;
+
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
+        SignInButton signInButton = (SignInButton) v.findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(this);
+        return v;
+
     }
 
     private void startSignIn(String customeToken) {
