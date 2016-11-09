978130ef4c45eb91128c5a71db1e95ff89c99280
Narasimha Gonapa <ngonapa@innominds.com>
2016-11-09 10:36:04 +0530

    Onboarding activity
    Initial commit

---
+++ app/src/store/java/in/gm/instaqueue/activity/StoreOnboarding.java
@@ -0,0 +1,31 @@
+package in.gm.instaqueue.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import in.gm.instaqueue.store.R;
+
+public class StoreOnboarding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_onboarding);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
