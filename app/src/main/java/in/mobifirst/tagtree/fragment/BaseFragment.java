4c29f91c58b3b05450888bc6ffd4b050a2167c2e
mouli1382 <mouli1382@gmail.com>
2016-11-12 14:58:49 +0530

    Refactored the package name - 2

---
+++ app/src/main/java/in/mobifirst/tagtree/fragment/BaseFragment.java
@@ -0,0 +1,34 @@
+package in.mobifirst.tagtree.fragment;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.view.View;
+
+public class BaseFragment extends Fragment {
+
+    public BaseFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public void showMessage(View view, int resId) {
+        Snackbar snackbar = Snackbar
+                .make(view, getString(resId), Snackbar.LENGTH_LONG);
+
+        snackbar.show();
+    }
+
+    public void showMessage(View view, String message) {
+        Snackbar snackbar = Snackbar
+                .make(view, message, Snackbar.LENGTH_LONG);
+
+        snackbar.show();
+    }
+}
