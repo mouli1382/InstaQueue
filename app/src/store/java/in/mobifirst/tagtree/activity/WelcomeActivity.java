90308ab23f1e500434e92b31734d19b3bf04a87c
Narasimha Gonapa <gnm444@gmail.com>
2016-11-14 00:16:02 +0530

    Internet connectivity check added in Welcome Activity
    Review the changes

--- app/src/store/java/in/mobifirst/tagtree/activity/WelcomeActivity.java
+++ app/src/store/java/in/mobifirst/tagtree/activity/WelcomeActivity.java
@@ -3,10 +3,17 @@ package in.mobifirst.tagtree.activity;
 import android.animation.Animator;
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.google.android.gms.common.SignInButton;
 
@@ -41,6 +48,31 @@ public class WelcomeActivity extends BaseActivity {
         setContentView(R.layout.activity_welcome);
         SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
         signInButton.setSize(SignInButton.SIZE_WIDE);
+
+        //final ViewGroup viewGroup = (ViewGroup) ((ViewGroup) this
+                //.findViewById(android.R.id.content)).getChildAt(0);
+
+        if (isInternetConnected() == false)
+            showDialog();
+    }
+
+    private void showDialog()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Application needs to connect to Wifi or MobileData")
+                .setCancelable(false)
+                .setPositiveButton("Connect to Wifi", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+                    }
+                })
+                .setNegativeButton("Connect to MobileData", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        startActivity(new Intent(Settings.ACTION_DATA_ROAMING_SETTINGS));
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 
     private void loadGoogleSignInActivity() {
@@ -56,6 +88,7 @@ public class WelcomeActivity extends BaseActivity {
     }
 
     private void bootUp() {
+
         if (mFirebaseAuth.getAuthInstance().getCurrentUser() != null) {
             Intent intent = new Intent(this, TokensActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_TASK_ON_HOME);
@@ -71,5 +104,14 @@ public class WelcomeActivity extends BaseActivity {
                 }
             });
         }
+    }
+
+    private boolean isInternetConnected()
+    {
+        ConnectivityManager cm = (ConnectivityManager) this.getSystemService(this.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+        return isConnected;
     }
 }
